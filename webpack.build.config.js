bdfa853c6e4657c8afdab906bf50461a8e26a43b
Arjan Einbu <arjan@einbu.no>
2017-04-16 15:36:33 +0200

    Fix absolute path requirement for output.path since webpack 2.3.0

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,10 +1,11 @@
+const path = require("path");
 const commonConfig = require("./webpack.common.config");
 const nodeExternals = require("webpack-node-externals");
 const merge = (...objs) => require("deepmerge").all(objs, {arrayMerge: (arr1, arr2) => arr1.concat(arr2) });
 
 const combinedConfig = merge({}, commonConfig, {
 	output: {
-		path: "./dist",
+		path: path.resolve(__dirname, "./dist"),
 		libraryTarget: "commonjs2",
 		filename: "bundle.js"
 	},
